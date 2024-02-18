6de092cdef2d5ff98dd55ad2fd1d88deae75f5e0
eslam <elbadry693@gmail.com>
2024-02-18 19:41:17 +0200

    add questions and replies

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    use HasFactory;
+    use HasFactory,Filterable;
     protected $fillable = ['user_id','question'];
 
     public function user()
